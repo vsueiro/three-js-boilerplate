2b976831ec605b9d06b04bfa1f1c171c567d131e
Vinicius Sueiro <hi@vsueiro.com>
2024-09-07 15:16:04 -0400

    Prevent click after dragging with mouse

--- class-version/scripts/Mouse.js
+++ class-version/scripts/Mouse.js
@@ -4,21 +4,53 @@ export default class Mouse {
   constructor(world) {
     this.world = world;
 
-    // Will always be a value between -1 and +1 in both axis
-    this.coords = new THREE.Vector2(-1, -1);
+    this.coords = new THREE.Vector2(0, 0);
+
+    this.range = [-1, 1];
+    this.diff = this.range[1] - this.range[0];
+    this.threshold = this.diff * 0.005;
+    this.dragging = false;
+    this.down = false;
 
     this.setup();
   }
 
-  handleMove(event) {
+  getCoords(event) {
     const { offsetX, offsetY } = event;
     const { width, height } = this.world;
+    const x = (offsetX / width) * this.diff - this.diff / 2;
+    const y = -(offsetY / height) * this.diff + this.diff / 2;
 
-    this.coords.x = (offsetX / width) * 2 - 1;
-    this.coords.y = -(offsetY / height) * 2 + 1;
+    return new THREE.Vector2(x, y);
   }
 
-  handleClick(event) {
+  handleDown(event) {
+    this.down = true;
+    this.prevCoords = this.getCoords(event);
+  }
+
+  handleMove(event) {
+    this.coords = this.getCoords(event);
+
+    if (this.down) {
+      const distance = this.prevCoords.distanceTo(this.coords);
+
+      if (distance > this.threshold) {
+        this.dragging = true;
+      }
+    }
+  }
+
+  handleUp(event) {
+    if (this.dragging === false) {
+      this.handleClick();
+    }
+
+    this.down = false;
+    this.dragging = false;
+  }
+
+  handleClick() {
     const closest = this.world.raycaster.closest("type", "cat");
 
     if (closest === undefined) {
@@ -29,13 +61,21 @@ export default class Mouse {
   }
 
   setup() {
+    this.world.element.addEventListener("mousedown", (event) => {
+      this.handleDown(event);
+    });
+
     this.world.element.addEventListener("mousemove", (event) => {
       this.handleMove(event);
     });
 
-    this.world.element.addEventListener("click", (event) => {
-      this.handleClick(event);
+    this.world.element.addEventListener("mouseup", (event) => {
+      this.handleUp(event);
     });
+
+    // this.world.element.addEventListener("click", (event) => {
+    //   this.handleClick(event);
+    // });
   }
 
   update() {}
